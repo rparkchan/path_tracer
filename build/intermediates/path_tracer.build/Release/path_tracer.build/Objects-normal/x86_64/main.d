7dbc3918ee90de5a60fb7297ac8f612583efec19
rparkchan <rparkchan@college.harvard.edu>
2019-01-28 13:43:34 -0500

    fixed untracked files

--- build/intermediates/path_tracer.build/Release/path_tracer.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/Ryan/Desktop/repositories/path_tracer/src/main.cpp \
-  /Users/Ryan/Desktop/repositories/path_tracer/src/cvec.h \
-  /Users/Ryan/Desktop/repositories/path_tracer/src/sdEstimators.h
